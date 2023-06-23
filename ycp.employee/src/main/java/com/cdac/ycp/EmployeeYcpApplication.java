85a6d7f1456845fd200d32414fdf44909b446fa8
sumit kumar <suvik6163@gmail.com>
2023-06-23 19:06:41 +0530

    java commit

---
+++ ycp.employee/src/main/java/com/cdac/ycp/EmployeeYcpApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.ycp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeYcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeYcpApplication.class, args);
+	}
+
+}
---
+++ ycp.employee/src/test/java/com/cdac/ycp/EmployeeYcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.ycp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeYcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
